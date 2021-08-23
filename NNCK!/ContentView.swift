3da917d5a93e704cb024af0a5006655ff6afde3b
enebin <46271447+enebin@users.noreply.github.com>
2021-08-23 11:26:57 +0900

    Initial Commit

---
+++ NNCK!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NNCK!
+//
+//  Created by 이영빈 on 2021/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NNCK!/NNCK_App.swift
@@ -0,0 +1,17 @@
+//
+//  NNCK_App.swift
+//  NNCK!
+//
+//  Created by 이영빈 on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NNCK_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
